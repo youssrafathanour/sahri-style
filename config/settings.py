9160f8b591cc57ecfcc6c4e5af611e05cf43493c
Youssera <youssrafathanour@gmail.com>
2026-09-11 00:09:12 +0200

    Fix indentation error in settings.py

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-
-    DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
